1ac32514045d7d075d3b7f5e5b302945b78786f1
Matheus-Pontes <62751571+Matheus-Pontes@users.noreply.github.com>
2021-07-19 11:52:21 -0300

    Update index.tsx
    Add title on page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <>
       <header>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Conversor de Medidas</title>
       </header>
     
       <main>
